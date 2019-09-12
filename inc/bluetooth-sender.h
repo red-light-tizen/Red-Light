99a3ab5c1ebe827059bc87cdc90495c9bb1fd67b
Seokjin Yoon <sjy5386@gmail.com>
2019-09-12 21:15:23 +0900

    Add new source file for input-getter

--- inc/bluetooth-sender.h
+++ inc/bluetooth-sender.h
@@ -15,10 +15,15 @@
 #define DATA_SENDING_CYCLE 1.0
 #define DATA_SIZE 62
 
+typedef struct bt_datatype{
+	char message[DATA_SIZE];
+	int length;
+}bt_data;
+
 void set_bluetooth_data_sending();
 void unset_bluetooth_data_sending();
-bt_error_e send_bluetooth_data(const char *, int);
+bt_error_e send_bluetooth_data(bt_data);
 static Eina_Bool bluetooth_send_timed_cb(void *);
-static void create_data_packet(char *);
+static void create_data_packet(bt_data*);
 
 #endif /* BLUETOOTH_SENDER_H_ */
