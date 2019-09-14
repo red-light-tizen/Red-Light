8c705d2b69d8c8ecd4cd7e27ff18592e62430d6a
Seokjin Yoon <sjy5386@gmail.com>
2019-09-14 17:58:50 +0900

    Add input-getter temporarily

---
+++ inc/input-getter.h
@@ -0,0 +1,34 @@
+/*
+ * input-getter.h
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#ifndef INPUT_GETTER_H_
+#define INPUT_GETTER_H_
+
+#include "red-light.h"
+
+/**
+ * All of "get_?" functions are returning last stored data in the buffer.
+ *
+ * TODO: Add buffer. (Hardware spec is required.)
+ */
+
+typedef struct body_datatype{
+	int pulse;
+	int temprature;
+	double spo2;
+	double activity;
+}body_data;
+
+typedef struct location_datatype{
+	double latitude;
+	double longitude;
+}location_data;
+
+body_data get_sensor_data();
+location_data get_location();
+
+#endif /* INPUT_GETTER_H_ */
---
+++ src/input-getter.c
@@ -0,0 +1,63 @@
+/*
+ * input-getter.c
+ *
+ *  Created on: Sep 12, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#include "input-getter.h"
+
+int get_pulse();
+double get_temperature();
+double get_spo2();
+double get_activity();
+
+
+body_data get_sensor_data(){
+	body_data datas;
+
+	datas.pulse = get_pulse();
+	datas.activity = get_activity();
+	datas.temprature = get_temperature();
+	datas.spo2 = get_spo2();
+
+	return datas;
+}
+
+int get_pulse() {
+	int pulse = 98;
+	_I("Pulse: %d", pulse);
+	return pulse;
+}
+
+double get_temperature() {
+	double temperature = 37.5;
+	_I("Temperature: %lf", temperature);
+	return temperature;
+}
+
+double get_spo2() {
+	double spo2 = 98.2;
+	_I("SPO2: %lf", spo2);
+	return spo2;
+}
+
+double get_activity() {
+	double activity = 23.0;
+	_I("Activity: %lf", activity);
+	return activity;
+}
+
+location_data get_location() {
+	double latitude = 36.23422;
+	_I("Latitude: %lf", latitude);
+	double longitude = -128.35869;
+	_I("Longitude: %lf", longitude);
+
+	location_data location;
+	location.latitude = latitude;
+	location.longitude = longitude;
+
+	return location;
+}
+
