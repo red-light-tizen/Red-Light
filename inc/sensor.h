3151f12f4c1d0d2cd1eb73d8af7d539d5bee4ac2
Seokjin Yoon <sjy5386@gmail.com>
2019-09-19 20:42:50 +0900

    Add new source file for sensor

---
+++ inc/sensor.h
@@ -0,0 +1,13 @@
+/*
+ * sensor.h
+ *
+ *  Created on: Sep 19, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#ifndef SENSOR_H_
+#define SENSOR_H_
+
+
+
+#endif /* SENSOR_H_ */
---
+++ src/sensor.c
@@ -0,0 +1,8 @@
+/*
+ * sensor.c
+ *
+ *  Created on: Sep 19, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#include "sensor.h"
