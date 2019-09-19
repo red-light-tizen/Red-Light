78d966b40aa5316dd2178579273e11ce0c3afad4
Kasania <nasang1997@gmail.com>
2019-09-19 11:55:31 +0900

    Add condition-indicator files

---
+++ inc/condition-indicator.h
@@ -0,0 +1,35 @@
+/*
+ * condition-indicator.h
+ *
+ *  Created on: Sep 19, 2019
+ *      Author: 나상혁
+ */
+
+#ifndef CONDITION_INDICATOR_H_
+#define CONDITION_INDICATOR_H_
+
+#include "utils.h"
+#include "event-bus.h"
+#include "input-getter.h"
+
+#define STATUS_NORMAL 0
+#define STATUS_CAUTION 1
+#define STATUS_DANGER 2
+
+typedef struct condition_datatype {
+	int status;
+
+	int pulse;
+	int temprature;
+	double spo2;
+	double activity;
+
+	double latitude;
+	double longitude;
+
+} condition_data;
+
+void checkCondition();
+int evaluateStatus();
+
+#endif /* CONDITION_INDICATOR_H_ */
---
+++ src/condition-indicator.c
@@ -0,0 +1,10 @@
+/*
+ * condition-indicator.c
+ *
+ *  Created on: Sep 19, 2019
+ *      Author: 나상혁
+ */
+
+#include "condition-indicator.h"
+
+
