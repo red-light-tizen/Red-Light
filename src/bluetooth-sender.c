3c12bc4b5684f1493568b340d5820ce4b7be70f5
Kasania <nasang1997@gmail.com>
2019-09-19 11:18:48 +0900

    Add bluetooth data struct.

--- src/bluetooth-sender.c
+++ src/bluetooth-sender.c
@@ -25,12 +25,12 @@ void unset_bluetooth_data_sending() {
 	}
 }
 
-bt_error_e send_bluetooth_data(const char *data, int length) {
+bt_error_e send_bluetooth_data(bt_data data) {
 	bt_error_e ret;
 	int client_socket_fd = get_bluetooth_socket_fd();
 
 	//ret is length of sent data
-	ret = bt_socket_send_data(client_socket_fd, data, length);
+	ret = bt_socket_send_data(client_socket_fd, data.message, data.length);
 	if (ret <= -1)
 	    dlog_print(DLOG_ERROR, LOG_TAG, "[bt_socket_send_data] failed.");
 
@@ -38,14 +38,15 @@ bt_error_e send_bluetooth_data(const char *data, int length) {
 }
 
 static Eina_Bool bluetooth_send_timed_cb(void *data) {
-	char my_data[DATA_SIZE];
-	create_data_packet(my_data);
-	send_bluetooth_data(my_data, strlen(my_data) + 1);
+//	char my_data[DATA_SIZE];
+	bt_data my_data;
+	create_data_packet(&my_data);
+	send_bluetooth_data(my_data);
 
 	return ECORE_CALLBACK_RENEW;
 }
 
-static void create_data_packet(char *data) {
+static void create_data_packet(bt_data *data) {
 	time_t t = time(NULL);
 	struct tm *timestamp = localtime(&t);
 	int year = timestamp->tm_year + 1900;
@@ -60,5 +61,6 @@ static void create_data_packet(char *data) {
 	double latitude = 37.482215;
 	double longitude = 126.885343;
 
-	snprintf(data, DATA_SIZE, "%d;%d;%d;%d;%d;%d;%d;%d;%f;%f;", year, day_of_year, second_of_day, condition, pulse, temperature, spo2, activity, latitude, longitude);
+	snprintf(data->message, DATA_SIZE, "%d;%d;%d;%d;%d;%d;%d;%d;%f;%f;", year, day_of_year, second_of_day, condition, pulse, temperature, spo2, activity, latitude, longitude);
+	data->length = strlen(data->message) + 1;
 }
