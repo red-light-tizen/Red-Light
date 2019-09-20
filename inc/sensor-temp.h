5f0fc1af7d4189bbcaf20cc72fb0103217adfa66
Seokjin Yoon <sjy5386@gmail.com>
2019-09-20 18:39:18 +0900

    Add new source file for sensor-temp

---
+++ inc/sensor-temp.h
@@ -0,0 +1,13 @@
+/*
+ * sensor-temp.h
+ *
+ *  Created on: Sep 20, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#ifndef SENSOR_TEMP_H_
+#define SENSOR_TEMP_H_
+
+#include "sensor.h"
+
+#endif /* SENSOR_TEMP_H_ */
---
+++ src/sensor-temp.c
@@ -0,0 +1,8 @@
+/*
+ * sensor-temp.c
+ *
+ *  Created on: Sep 20, 2019
+ *      Author: Seokjin Yoon
+ */
+
+#include "sensor-temp.h"
